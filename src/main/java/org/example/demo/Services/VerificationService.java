dd6ed26bb8ff47022cd9944bfb93387989b9541f
hungdang2905 <20130271@st.hcmuaf.edu.vn>
2023-12-22 12:28:02 +0700

    ý tưởng admin check hóa đơn đã xác nhận

---
+++ src/main/java/org/example/demo/Services/VerificationService.java
@@ -0,0 +1,13 @@
+package org.example.demo.Services;
+
+import dao.SignDao;
+import dao.VerificationDao;
+import model.Account;
+import model.Sign;
+import model.Verification;
+
+public class VerificationService {
+    public static long add(Verification verification) {
+        return VerificationDao.add(verification);
+    }
+}
