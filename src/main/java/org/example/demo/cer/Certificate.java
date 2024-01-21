0535b676f4b77ac57738bf4c39455abb605f581b
hungdang2905 <20130271@st.hcmuaf.edu.vn>
2024-01-21 12:25:53 +0700

    cer

---
+++ src/main/java/org/example/demo/cer/Certificate.java
@@ -0,0 +1,24 @@
+package org.example.demo.cer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.InputStream;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Certificate implements Serializable {
+    private Integer userId;
+    private Timestamp startDate;
+    private Timestamp endDate;
+    private InputStream data;
+    private String status;
+    private Integer id;
+    private Timestamp created_at;
+    private Timestamp update_at;
+
+}
---
+++ src/main/java/org/example/demo/cer/DownloadCerAPI.java
@@ -0,0 +1,43 @@
+package org.example.demo.cer;
+
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import org.example.demo.dao.SignDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet(value = "/download-cer")
+public class DownloadCerAPI extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        Certificate oneByCerId = SignDao.findOneByCerId(Integer.parseInt(id));
+        try {
+            // Set response headers for PDF download
+            response.setContentType("application/pkcs12");
+            response.setHeader("Content-Disposition", "attachment; filename=\"cer-" + id + ".jks\"");
+
+            // Read the entire input stream into a byte array
+            byte[] dataBytes = oneByCerId.getData().readAllBytes();
+            // Send the byte array as the response output stream
+            response.getOutputStream().write(dataBytes);
+        } catch (IOException e) {
+            e.printStackTrace();  // Consider logging the error
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+}
+
